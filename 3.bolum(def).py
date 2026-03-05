51226e4d9d4ef62df40276c203b2dee8cfb0fd2b
inconnu-xx <bosislerrmuduru34@gmail.com>
2026-03-05 20:28:18 +0300

    Update and rename 3.bolum_def_.py to 3.bolum(def).py

--- 3.bolum(def).py
+++ 3.bolum(def).py
@@ -6,7 +6,21 @@ hobi = input('hobiniz: ')
 
 print(f"eleman adı: {isim}\neleman yası: {yas}\neleman mesleği: {meslek}\neleman hobisi: {hobi}")
 
-#--------------------
+# --------------------
+
+# parametresiz fonksiyon örn.
+
+def topla():
+    birinci_sayi = int(input("taplanacak ilk sayı: "))
+    ikinci_sayi = int(input("toplanacak ikinci sayı :"))
+    islem = birinci_sayi + ikinci_sayi
+    print(islem)
+
+topla()
+
+# --------------------
+
+# parametreli fonksiyon örn.
 
 def bilgi_al(ad, spor, meslek, hobi):
     print(f"eleman adı: {ad}\neleman sporu: {spor}\neleman mesleği: {meslek}\neleman hobisi: {hobi}")
@@ -15,6 +29,19 @@ def bilgi_al(ad, spor, meslek, hobi):
 bilgi_al("esam", "callisthenics", "satış", "gitar")
 
 # --------------------
+
+# basit su hesaplama
+
+def su_hesap_basit():
+    kilo = int(input("kilonuzu giriniz: "))
+    islem = kilo * 0.03
+    print("icmeniz gereken su miktarı: ", islem)
+
+su_hesap_basit()
+
+# ---------------------
+
+# parametreli ve cinsiyete göre su hesaplama
 
 def su_hesapla(kilo):
     e_hesapla = kilo*0.04
@@ -39,3 +66,14 @@ def su_hesapla(kilo):
 kilo_al = int(input("kilonuzu giriniz: "))
 
 su_hesapla(kilo_al)
+
+# kilo parametresini kiloal ile dışarıdan alıyoruz kodumuz bunun üzerine kurulu
+# sadelik icin hesap değişkenlerini icerden aldık ama dışardanda alabilirdik
+
+# ----------------------
+
+
+
+
+
+
